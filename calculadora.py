c48b8acec9429b7cf6abf17eb71ab01e51dcf484
Tel e Ju <teonildo@gmail.com>
2024-02-19 14:51:47 -0300

    Import biblioteca

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 import flet as ft
 from flet import colors
 from decimal import Decimal
-import math
+
 
 
 # Lista de botões da calculadora, com informações sobre operadores, cores de fonte e fundo.
